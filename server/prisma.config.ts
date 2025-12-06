8f095a5504480926bc0512f889e4d2ade5b53f2a
random-ization <ssunhr@gmail.com>
2025-12-07 05:19:36 +0900

    Add Prisma configuration file

---
+++ server/prisma.config.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+  migrate: {
+    adapter: async () => {
+      const url = process. env.DATABASE_URL
+      if (!url) throw new Error('DATABASE_URL is required')
+      return { url }
+    }
+  }
+})

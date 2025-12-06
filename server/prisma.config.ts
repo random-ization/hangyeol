ebe5215bfe8b4956c2a9919619736f7c55f7fc72
random-ization <ssunhr@gmail.com>
2025-12-06 21:58:41 +0000

    Fix spacing in DATABASE_URL environment variable

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
   migrate: {
     adapter: async () => {
-      const url = process. env.DATABASE_URL
+      const url = process.env.DATABASE_URL
       if (!url) throw new Error('DATABASE_URL is required')
       return { url }
     }
